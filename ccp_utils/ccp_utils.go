f2f9933375379910f479e26be85edcb9389ce0dc
dåshuång li <li.dashuang@icloud.com>
2020-05-25 20:34:58 +0800

    feat: file upload api
    ## files create
    curl -X "POST" "http://localhost:8080/files/create" \
         -H 'Content-Type: multipart/form-data; charset=utf-8; boundary=__X_PAW_BOUNDARY__' \
         -F "file="

--- ccp_utils/ccp_utils.go
+++ ccp_utils/ccp_utils.go
@@ -1,10 +1,14 @@
 package ccp_utils
 
 import (
+	"io"
+	"net/http"
 	"os"
 
 	ccpsdk "github.com/alibabacloud-go/ccppath-sdk/client"
 )
+
+var runtimeOptions = new(ccpsdk.RuntimeOptions).SetMaxAttempts(2)
 
 func ListFiles(akClient *ccpsdk.Client) (*ccpsdk.CCPListFileResponse, error) {
 	listFileRequestModel := new(ccpsdk.ListFileRequestModel)
@@ -15,13 +19,62 @@ func ListFiles(akClient *ccpsdk.Client) (*ccpsdk.CCPListFileResponse, error) {
 
 	listFileRequestModel.SetBody(listFileRequest)
 
-	runtimeOptions := new(ccpsdk.RuntimeOptions).SetMaxAttempts(2)
-
 	response, err := akClient.ListFile(listFileRequestModel, runtimeOptions)
 	if err != nil {
 		return nil, err
-	} else {
-		return response.Body, nil
 	}
 
+	return response.Body, nil
+
+}
+
+func CreateFile(akClient *ccpsdk.Client, filename string) (*ccpsdk.CCPCreateFileResponse, error) {
+	createFileModel := new(ccpsdk.CreateFileRequestModel)
+	createFileRequest := new(ccpsdk.CCPCreateFileRequest).
+		SetDriveId(os.Getenv("DRIVE_ID")).
+		SetName(filename).
+		SetType("file").
+		SetParentFileId("root").
+		SetContentType("text/plain")
+	createFileModel.SetBody(createFileRequest)
+
+	response, err := akClient.CreateFile(createFileModel, runtimeOptions)
+	if err != nil {
+		return nil, err
+	}
+	return response.Body, nil
+}
+
+func CompleteFile(akClient *ccpsdk.Client, createFileBody *ccpsdk.CCPCreateFileResponse, content io.Reader) (*ccpsdk.CCPCompleteFileResponse, error) {
+	uploadUrl := createFileBody.PartInfoList[0].UploadUrl
+	uploadId := createFileBody.UploadId
+	fileId := createFileBody.FileId
+
+	req, _ := http.NewRequest("PUT", *uploadUrl, content)
+	req.Header.Add("Content-Type", "")
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	etag := res.Header.Get("ETag")
+
+	uploadPartInfo := new(ccpsdk.UploadPartInfo)
+	uploadPartInfo.SetEtag(etag)
+	uploadPartInfo.SetPartNumber(1)
+
+	completeFileModel := new(ccpsdk.CompleteFileRequestModel)
+	completeFileRequest := new(ccpsdk.CCPCompleteFileRequest).
+		SetDriveId(os.Getenv("DRIVE_ID")).
+		SetFileId(*fileId).
+		SetUploadId(*uploadId).
+		SetPartInfoList([]*ccpsdk.UploadPartInfo{uploadPartInfo})
+	completeFileModel.SetBody(completeFileRequest)
+
+	completeResponse, err := akClient.CompleteFile(completeFileModel, runtimeOptions)
+	if err != nil {
+		return nil, err
+	}
+	return completeResponse.Body, nil
 }
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"ccp-tool/ccp_utils"
 	"log"
+	"net/http"
 	"os"
+	"path/filepath"
 
 	ccpsdk "github.com/alibabacloud-go/ccppath-sdk/client"
 	"github.com/gin-gonic/gin"
@@ -33,14 +35,40 @@ func setupRouter() *gin.Engine {
 	r.Use(gin.Logger())
 
 	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
+		c.String(http.StatusOK, "pong")
 	})
 
 	r.GET("/files", func(c *gin.Context) {
 		if body, err := ccp_utils.ListFiles(akClient); err != nil {
-			c.JSON(400, gin.H{"err_msg": err.Error()})
+			c.JSON(http.StatusBadRequest, gin.H{"err_msg": err.Error()})
 		} else {
-			c.JSON(200, body)
+			c.JSON(http.StatusOK, body)
+		}
+	})
+
+	r.POST("/files/create", func(c *gin.Context) {
+		fileHeader, err := c.FormFile("file")
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "need file params"})
+			return
+		}
+
+		filename := filepath.Base(fileHeader.Filename)
+		file, err := fileHeader.Open()
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "file open error"})
+			return
+		}
+
+		if createFileBody, err := ccp_utils.CreateFile(akClient, filename); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		} else {
+			if completeFileBody, err := ccp_utils.CompleteFile(akClient, createFileBody, file); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			} else {
+				c.JSON(http.StatusOK, completeFileBody)
+			}
 		}
 	})
 
